ddb89ef416e42969c4291a24bc22755a7f8e98ee
Alexey Steklov <steklovalexey@gmail.com>
2018-06-12 17:17:13 +0400

    Command.php

--- src/Command.php
+++ src/Command.php
@@ -8,12 +8,18 @@
 
 namespace alexeysteklov\yii\queue\pgq;
 
+use yii\console\Exception;
 
 class Command extends \yii\queue\cli\Command
 {
 
 
     public $isolate = false;
+
+    /**
+     * @var Queue
+     */
+    public $queue;
 
     /**
      * @param string $actionID
@@ -24,4 +30,35 @@ class Command extends \yii\queue\cli\Command
     {
         return in_array($actionID, ['run', 'listen'], true);
     }
+
+    /**
+     * Runs all jobs from db-queue.
+     * It can be used as cron job.
+     *
+     * @return null|int exit code.
+     */
+    public function actionRun()
+    {
+        return $this->queue->run(false);
+    }
+
+    /**
+     * Listens db-queue and runs new jobs.
+     * It can be used as daemon process.
+     *
+     * @param int $timeout number of seconds to sleep before next reading of the queue.
+     * @throws Exception when params are invalid.
+     * @return null|int exit code.
+     */
+    public function actionListen($timeout = 3)
+    {
+        if (!is_numeric($timeout)) {
+            throw new Exception('Timeout must be numeric.');
+        }
+        if ($timeout < 1) {
+            throw new Exception('Timeout must be greater that zero.');
+        }
+        return $this->queue->run(true, $timeout);
+    }
+
 }
